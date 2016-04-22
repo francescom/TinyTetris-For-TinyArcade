2b63f6389978316cab6f8c709159876cac035143
Francesco Munafò <fmm@ZenRaid.local>
2016-04-22 12:59:01 +0200

    Audio files

---
+++ AudioSources/04 exportedCCode/invbell.h
@@ -0,0 +1,11 @@
+//
+// Generated from source file '02 rawSnds/invbell.raw'
+//
+
+#ifndef __invbell_h
+#define __invbell_h
+
+extern "C" const unsigned char invbell[];
+
+#endif // __invbell_
+
---
+++ AudioSources/04 exportedCCode/pff.h
@@ -0,0 +1,11 @@
+//
+// Generated from source file '02 rawSnds/pff.raw'
+//
+
+#ifndef __pff_h
+#define __pff_h
+
+extern "C" const unsigned char pff[];
+
+#endif // __pff_
+
